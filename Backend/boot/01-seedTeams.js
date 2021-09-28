a2bc230b6a65bdd0cbc2adbc3efe0c0954c47e61
Yahya Wsea <yahya.wsea@fixedmea.com>
2021-09-28 10:56:13 +0200

    fixing issue in seeding

--- Backend/boot/01-seedTeams.js
+++ Backend/boot/01-seedTeams.js
@@ -1,22 +1,38 @@
 const Team = require('../models/Team');
+const User = require('../models/User');
 
 const { logger } = require('../logger/logger');
 
 module.exports = async function seedTeams() {
   const team1 = await Team.findOne({ name: 'team1' });
   if (!team1) {
-    await Team.create({
+    const createdTeam1 = await Team.create({
       name: 'team1',
     });
-    logger.info(`Team1 ctreated `);
+    logger.info(`Team1 ctreated with id: ${createdTeam1._id}`);
+    await User.create({
+      name: 'player1',
+      teamId: createdTeam1._id
+    });
+    await User.create({
+      name: 'player2',
+      teamId: createdTeam1._id
+    });
   }
 
   const team2 = await Team.findOne({ name: 'team2' });
-  // logger.info(`Team ${team2.name} already exists `);
   if (!team2) {
-    await Team.create({
+    const createdTeam2 = await Team.create({
       name: 'team2',
     });
-    logger.info(`Team2 ctreated `);
+    logger.info(`Team2 ctreated with id: ${createdTeam2._id}`);
+    await User.create({
+      name: 'player3',
+      teamId: createdTeam2._id,
+    });
+    await User.create({
+      name: 'player4',
+      teamId: createdTeam2._id,
+    });
   }
 };
--- Backend/routes/board.routes.js
+++ Backend/routes/board.routes.js
@@ -6,6 +6,8 @@ const User = require('../models/User');
 const Team = require('../models/Team');
 const Board = require('../models/Board');
 
+const { NOT_FOUND_ERR } = require('../error/errors')
+
 defaultBoard = {
   c1: 8,
   c2: 6,
@@ -22,22 +24,27 @@ boardRouter.get(
   '/',
   asyncTryCatch(async (req, res) => {
     const { userName, teamName } = req.query;
+    console.log({ userName, teamName });
     const team = await Team.findOne({ name: teamName }).lean();
     const user = await User.findOne({ name: userName }).lean();
-    if (user.teamId.toString() === team._id.toString()) {
-      const allBoards = await Board.find();
-      console.log({ allBoards });
-      const board = await Board.findOne({
-        teamId: team._id.toString(),
-        latest: true,
-      });
-      console.log({ board });
-      if (board) {
-        res.send(board);
-      } else {
-        console.log({ defaultBoard });
-        res.send({ ...defaultBoard, teamId: team._id });
+    if (user && team) {
+      if (user.teamId.toString() === team._id.toString()) {
+        const allBoards = await Board.find();
+        console.log({ allBoards });
+        const board = await Board.findOne({
+          teamId: team._id.toString(),
+          latest: true,
+        });
+        console.log({ board });
+        if (board) {
+          res.send(board);
+        } else {
+          console.log({ defaultBoard });
+          res.send({ ...defaultBoard, teamId: team._id });
+        }
       }
+    } else {
+      throw NOT_FOUND_ERR;
     }
   })
 );
