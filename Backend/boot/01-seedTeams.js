1a01afc031b551102b6a5a6fa19d26a7535caa81
Yahya Wsea <yahya.wsea@fixedmea.com>
2021-09-28 06:25:35 +0200

    hot config fix

--- Backend/boot/01-seedTeams.js
+++ Backend/boot/01-seedTeams.js
@@ -4,18 +4,19 @@ const { logger } = require('../logger/logger');
 
 module.exports = async function seedTeams() {
   const team1 = await Team.findOne({ name: 'team1' });
-  logger.info(`Team ${team1.name} already exists `);
   if (!team1) {
     await Team.create({
       name: 'team1',
     });
+    logger.info(`Team1 ctreated `);
   }
 
   const team2 = await Team.findOne({ name: 'team2' });
-  logger.info(`Team ${team2.name} already exists `);
+  // logger.info(`Team ${team2.name} already exists `);
   if (!team2) {
     await Team.create({
       name: 'team2',
     });
+    logger.info(`Team2 ctreated `);
   }
 };
